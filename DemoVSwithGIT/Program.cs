91089c7cf506e5d463ce6d53196238bcad1a0555
romil <romil.chandragade@aquariansoft.in>
2014-12-01 16:45:09 +0530

    final

--- DemoVSwithGIT/Program.cs
+++ DemoVSwithGIT/Program.cs
@@ -20,6 +20,13 @@ namespace DemoVSwithGIT
             Console.WriteLine("Hello Thia is Demo Display Functiion..");
             Console.WriteLine("Hello Thia is Demo Display Functiion..");
             Console.WriteLine("Hello");
+
+
+        }
+
+        public static void DisplayMsg()
+        {
+            Console.WriteLine("how is this possoble..?");
         }
     }
 }
