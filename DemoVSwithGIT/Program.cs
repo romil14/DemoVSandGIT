1ca364aac74d2d3eabb36129c89191e4592c6b76
romil <romil.chandragade@aquariansoft.in>
2014-12-01 15:39:08 +0530

    Changes1

---
+++ DemoVSwithGIT/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DemoVSwithGIT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World");
+            Display();
+            Console.ReadKey();
+        }
+
+        public static void Display()
+        {
+            Console.WriteLine("Hello Thia is Demo Display Functiion..");
+            Console.WriteLine("Hello Thia is Demo Display Functiion..");
+            Console.WriteLine("Hello Thia is Demo Display Functiion..");
+        }
+    }
+}
